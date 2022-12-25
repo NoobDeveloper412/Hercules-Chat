38261b4e24ad962cdab585b7beffe676c70f29a0
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-12-25 12:07:56 +0500

    Fixed the store issues, turned out to be that I forgot to use () when calling the composeWithDevTools()

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import * as actionTypes from "./types";
 
-export const setUser = (user) => {
+export const setUser = user => {
   return {
     type: actionTypes.SET_USER,
     payload: {
-      currentUser: user,
-    },
+      currentUser: user
+    }
   };
 };
