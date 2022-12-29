80fb0523c328c0c02fd5c326b710991e46a34b22
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-12-29 22:39:13 +0500

    Added the color picker modal.

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { Button, Divider, Menu, Sidebar } from "semantic-ui-react";
+import {
+  Button,
+  Divider,
+  Menu,
+  Sidebar,
+  Modal,
+  Icon,
+  Label,
+} from "semantic-ui-react";
+
+import { TwitterPicker } from "react-color";
 
 class ColorPanel extends React.Component {
+  state = {
+    modal: false,
+  };
+  toggleModal = () => {
+    this.setState({ modal: !this.state.modal });
+  };
   render() {
+    const { modal } = this.state;
     return (
       <Sidebar
         as={Menu}
@@ -13,7 +30,44 @@ class ColorPanel extends React.Component {
         width="very thin"
       >
         <Divider />
-        <Button icon="add" color="blue" size="small" />
+        <Button
+          icon="add"
+          color="blue"
+          size="small"
+          onClick={this.toggleModal}
+        />
+
+        {/* Color Picker */}
+        <Modal dimmer="blurring" transition="scale" open={modal} size="small"> 
+          <Modal.Header>Choose App Colors</Modal.Header>
+          <Modal.Content
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ margin: "10px" }}>
+              <Label content="Primary Color" />
+              <TwitterPicker className="twitterPicker" />
+            </div>
+            <div style={{ margin: "10px" }}>
+              <Label content="Secondary Color"  />
+              <TwitterPicker className="twitterPicker" />
+            </div>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" inverted>
+              <Icon name="checkmark" />
+              Save Colors
+            </Button>
+            <Button color="red" inverted onClick={this.toggleModal}>
+              <Icon name="remove" />
+              Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </Sidebar>
     );
   }
