3436ad3e60e791a19f96e2a6048a7d51027fcf95
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-12-29 22:02:58 +0500

    Added  the accordions of the meta panel and displayed the channels info in the meta panel.

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import MetaPanel from "./MetaPanel.js/MetaPanel";
 import SidePanel from "./SidePanel/SidePanel";
 // import { ToastContainer } from "react-toastify";
 
-const App = ({ currentUser, currentChannel,isPrivateChannel }) => {
+const App = ({ currentUser, currentChannel, isPrivateChannel }) => {
   return (
     <React.Fragment>
       <Grid columns="equal" className="app" style={{ background: "#eee" }}>
@@ -26,7 +26,11 @@ const App = ({ currentUser, currentChannel,isPrivateChannel }) => {
           />
         </Grid.Column>
         <Grid.Column width={4}>
-          <MetaPanel />
+          <MetaPanel
+            key={currentChannel && currentChannel.id}
+            isPrivateChannel={isPrivateChannel}
+            currentChannel={currentChannel}
+          />
         </Grid.Column>
       </Grid>
       {/* <ToastContainer /> */}
@@ -36,6 +40,6 @@ const App = ({ currentUser, currentChannel,isPrivateChannel }) => {
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
-  isPrivateChannel:state.channel.isPrivateChannel
+  isPrivateChannel: state.channel.isPrivateChannel,
 });
 export default connect(mapStateToProps)(App);
